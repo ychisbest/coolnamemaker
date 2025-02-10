a4988f9f5256b1c3be0c7982578ae167877dc2e7
YCH <943696882@qq.com>
2025-02-10 14:33:15 +0800

    add large pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
   site: 'https://www.coolnamemaker.com',
 });
-

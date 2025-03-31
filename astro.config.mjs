8c708ff7a20314fa11cf779d35d503a0e2e822ca
YCH <943696882@qq.com>
2025-03-31 14:51:09 +0800

    big change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,43 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
+
+import tailwindcss from '@tailwindcss/vite';
+
+
+import icon from 'astro-icon';
+
 
 import react from '@astrojs/react';
 
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
+  // Set your site's URL
+  site: 'https://coolnamemaker.com',
+
+  i18n: {
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
   },
 
-  integrations: [sitemap(
+  integrations: [mdx(), sitemap({
+    i18n: {
+      defaultLocale: DEFAULT_LOCALE_SETTING,
+      locales: Object.fromEntries(
+        Object.entries(LOCALES_SETTING).map(
+          ([key, value]) => [key, value.lang ?? key]
+        )
+      ),
+    },
+  }), icon(), react()],
 
-    {
-      filter: (page) => {
-        return !page.includes('/page');
-      }
-    }
-
-  ), react()],
-  site: 'https://www.coolnamemaker.com',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ src/consts.ts
@@ -0,0 +1,31 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+import type { Multilingual } from "@/i18n";
+
+export const SITE_TITLE: string | Multilingual = "Cool Name Generator";
+
+
+export const SITE_DESCRIPTION: string | Multilingual = {
+	en: "Generate unique and creative names for your projects, characters, businesses, or brands. Find the perfect cool name with our easy-to-use name generator.",
+	es: "Genera nombres únicos y creativos para tus proyectos, personajes, negocios o marcas. Encuentra el nombre perfecto con nuestro generador de nombres fácil de usar.",
+	ar: "قم بإنشاء أسماء فريدة وإبداعية لمشاريعك أو شخصياتك أو أعمالك أو علاماتك التجارية. اعثر على الاسم المثالي باستخدام مولد الأسماء سهل الاستخدام.",
+	pt: "Gere nomes únicos e criativos para seus projetos, personagens, negócios ou marcas. Encontre o nome perfeito com nosso gerador de nomes fácil de usar.",
+	fr: "Générez des noms uniques et créatifs pour vos projets, personnages, entreprises ou marques. Trouvez le nom parfait avec notre générateur de noms facile à utiliser.",
+	ru: "Создавайте уникальные и креативные имена для ваших проектов, персонажей, бизнеса или брендов. Найдите идеальное имя с помощью нашего простого генератора имен.",
+	"zh-cn": "为您的项目、角色、企业或品牌生成独特而创意的名称。使用我们易于使用的名称生成器找到完美的名称。",
+	ja: "あなたのプロジェクト、キャラクター、ビジネス、ブランドのためのユニークで創造的な名前を生成します。使いやすい名前ジェネレーターで完璧なクールネームを見つけましょう。",
+};
+
+export const X_ACCOUNT: string | Multilingual = "@psephopaiktes";
+
+export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
+	en: "This page is not available in your language.",
+	es: "Esta página no está disponible en tu idioma.",
+	ar: "هذه الصفحة غير متوفرة بلغتك.",
+	pt: "Esta página não está disponível no seu idioma.",
+	fr: "Cette page n'est pas disponible dans votre langue.",
+	ru: "Эта страница недоступна на вашем языке.",
+	"zh-cn": "此页面不支持您的语言。",
+	ja: "このページはご利用の言語でご覧いただけません。",
+};
